30460f54b29778401c8be76e31850c9197a7c9a1
frealexandro <jhoner300@gmail.com>
2025-05-27 15:39:27 -0500

    i complete te first ecersice

---
+++ #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO.py
@@ -0,0 +1,67 @@
+# /*
+#  * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+#  * - Recuerda que todas las instrucciones de participación están en el
+#  *   repositorio de GitHub.
+#  *
+#  * Lo primero... ¿Ya has elegido un lenguaje?
+#  * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+#  * - Este primer reto te servirá para familiarizarte con la forma de participar
+#  *   enviando tus propias soluciones.
+#  *
+#  * EJERCICIO:
+#  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+#  *   lenguaje de programación que has seleccionado.
+#  * - Representa las diferentes sintaxis que existen de crear comentarios
+#  *   en el lenguaje (en una línea, varias...).
+#  * - Crea una variable (y una constante si el lenguaje lo soporta).
+#  * - Crea variables representando todos los tipos de datos primitivos
+#  *   del lenguaje (cadenas de texto, enteros, booleanos...).
+#  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+#  *
+#  * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+#  * debemos comenzar por el principio.
+#  */
+
+
+#all : primer comentario
+
+
+#! https://www.python.org/
+
+#all: segunda forma de hacer comentarios
+
+"""esto es un comentario de varias lineas de python"""
+
+#all: declaracion de variables y constantes
+
+my_variale = "variable_1"
+
+my_constante = "my_constante_1"
+
+#all: tipos de variables de datos primitivos del lenguaje
+
+#! texto 
+
+texto = 'hola undo'
+
+#! entero
+
+numero = 23
+
+#! flotante
+
+numero_decimal = 20.5
+
+#! boleano 
+
+es_humano = True
+
+#all: imprimir hola y el nombre del lenguaje
+
+print ( "¡Hola, Python!")
+
+
+
+
+
+
