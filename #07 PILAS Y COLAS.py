8d8f530bccec504e6e0dea3a3829b8c4ed33e196
frealexandro <jhoner300@gmail.com>
2025-08-25 16:39:14 -0500

    i complete the challenge and the excersice of the stack and the queue

---
+++ #07 PILAS Y COLAS.py
@@ -0,0 +1,128 @@
+
+
+# /*
+#  * EJERCICIO:
+#  * Implementa los mecanismos de introducción y recuperación de elementos propios de las
+#  * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
+#  * o lista (dependiendo de las posibilidades de tu lenguaje).
+#  *
+#  * DIFICULTAD EXTRA (opcional):
+#  * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+#  *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+#  *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+#  *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+#  *   el nombre de una nueva web.
+#  * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+#  *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+#  *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+#  *   interpretan como nombres de documentos.
+#  */
+
+
+
+
+print("\n pilas fifo \n")
+
+#!  pila/lifo (last in first out)
+
+stack = []
+
+#! push
+
+stack.append("1")
+stack.append("2")
+stack.append("3")
+
+print(stack)
+
+#! pop
+
+stack_item = stack[len(stack) - 1]
+del stack[len(stack) - 1]
+
+print(stack_item)
+
+
+print("\n colas fifo \n")
+#! cola/fifo (first in , first out)
+
+
+queue = []
+
+#! enqueue
+queue.append(1)
+queue.append(2)
+queue.append(3)
+
+#! dequeue
+
+queue_item = queue[0]
+del queue[0]
+print(queue_item)
+
+print(queue.pop(0))
+
+print(queue)
+
+#  * DIFICULTAD EXTRA (opcional):
+#  * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+#  *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+#  *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+#  *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+#  *   el nombre de una nueva web.
+#  * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+#  *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+#  *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+#  *   interpretan como nombres de documentos.
+#  */
+
+
+def web_navigation():
+
+    stack = []
+
+    while True:
+        
+        action = input(
+            "Añade una url o interactua con las palabras adelante/atras/salir: "
+                       )
+        
+        if action == "salir" :
+            print("saliendo del navegador web")
+            break
+        if action == "adelante":
+            pass
+        if action == "atras":
+            if len(stack) > 0 :
+                stack.pop()
+        else:
+            stack.append(action)
+
+        if len(stack) > 0:
+            print(f"Has navegado a la web: {stack[len(stack) - 1]}")
+        else:
+            print("Estas en la pagina de inicio")
+
+
+#web_navigation()
+
+def shared_printed():
+
+    queue = []
+
+    while True:
+
+        action = input("añade un documento o selecciona imprimir/salir: ")
+    
+        if action == "salir":
+            break
+        elif action == "imprimir":
+            if len(queue) > 0 :
+                print(f"se va a imprimir el elemento {queue.pop(0)}")
+
+        else:
+            queue.append(action)
+
+        print(f"Cola de impresion: {queue}")    
+
+shared_printed()
